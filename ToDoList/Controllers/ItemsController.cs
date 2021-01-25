5826f82e36a0c78df49885d5b1c43f3270c21451
Travis Scott <travisty12@gmail.com>
2021-01-24 23:13:18 -0800

    update create method to save parent id before join entiity creation

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -31,6 +31,7 @@ namespace ToDoList.Controllers
     public ActionResult Create(Item item, int CategoryId)
     {
       _db.Items.Add(item);
+      _db.SaveChanges();
       if (CategoryId != 0)
       {
         _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
